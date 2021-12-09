d2a04347add221496fb9af177b6a32c68d776978
juliamontenegro <juliamontenegro1402@gmail.com>
2021-12-08 21:48:26 -0300

    Componente início sem consumo

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { UserLogin } from '../model/UserLogin';
 import { Usuario } from '../model/Usuario';
 
@@ -9,16 +10,26 @@ import { Usuario } from '../model/Usuario';
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-    private http: HttpClient
+    
 
-  entrar (userLogin:UserLogin): Observable<UserLogin> {
+  entrar(userLogin:UserLogin): Observable<UserLogin> {
       return this.http.post<UserLogin>("http://localhost:8080/usuarios/logar", userLogin)
   }
 
-  cadastrar (Usuario: Usuario): Observable<Usuario> {
-      return this.http.post<Usuario>("http://localhost:8080/usuarios/cadastrar", Usuario)
+  cadastrar(usuario: Usuario): Observable<Usuario> {
+      return this.http.post<Usuario>("http://localhost:8080/usuarios/cadastrar", usuario)
+  }
+
+  logado(){
+    let ok: boolean = false
+
+    if (environment.token != ''){
+      ok = true
+    }
+
+    return ok
   }
 
 }
